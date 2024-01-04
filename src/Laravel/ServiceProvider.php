10ae34f23b8b1409ef9795d37f80a3fcf168d41e
hugh.li <hugh.li@foxmail.com>
2024-01-04 09:43:12 +0800

    feat: 性能优化

--- src/Laravel/ServiceProvider.php
+++ src/Laravel/ServiceProvider.php
@@ -11,16 +11,18 @@ namespace HughCube\Profiler\Laravel;
 use HughCube\Profiler\HProfiler;
 use HughCube\Profiler\Profiler;
 use Illuminate\Config\Repository;
+use Illuminate\Contracts\Container\BindingResolutionException;
 
 class ServiceProvider extends \Illuminate\Support\ServiceProvider
 {
     /**
      * Register the provider.
+     * @throws BindingResolutionException
      */
     public function register()
     {
         /** @var Repository $config */
-        $config = $this->app['config'];
+        $config = $this->app->make('config');
 
         HProfiler::setRootProfiler(new Profiler($config->get('profiler')));
     }
